97083be8b2ae9efb711af9a2f5f31159c9070452
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-05 12:47:57 +0300

    added the list group sorting function

--- src/components/common/listgroup.jsx
+++ src/components/common/listgroup.jsx
@@ -1,13 +1,32 @@
 import React from 'react'
 
-const ListGroup = () => {
+const ListGroup = props => {
+    const {textProperty,
+        valueProperty,
+        items,
+        onItemSelected,
+        selectedItem
+    } = props;
     return (
         <div>
             <ul className="list-group">
-                <li className="list-group-item">{}</li>
+                {items.map(item=>
+                <li onClick={()=>onItemSelected(item)} 
+                key={item[valueProperty]} 
+                className={item===selectedItem?
+                    "list-group-item active":
+                     "list-group-item"}
+                >
+                {item[textProperty]}
+                </li>
+)}
             </ul>
         </div>
       );
+};
+ListGroup.defaultProps ={
+    textProperty:'name',
+    valueProperty:'_id'
 }
  
 export default ListGroup;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -8,11 +8,14 @@ import { getGenres } from '../services/fakeGenreService';
 import ListGroup from './common/listgroup';
 class Movies extends Component {
     state = { 
-        movies:getMovies(),
+        movies:[],
         pageSize:4,
         currentPage:1,
-        movieGenre:getGenres()
+        genres:[]
      } 
+     componentDidMount(){
+        this.setState({movies:getMovies(),genres:getGenres() })
+     }
      handleDelete=(movie)=>{
         let movies=this.state.movies.filter(m=>m._id!==movie._id)
         this.setState({movies})
@@ -24,26 +27,34 @@ class Movies extends Component {
        movies[index].liked = ! movies[index].liked
        this.setState({movies})
      }
-     handlePageChange=(page)=>{
+     handlePageChange=page=>{
         this.setState({currentPage:page})
-
      }
-     
-    
-     
+     handleGenreSelected=genre=>{
+        this.setState({selectedGenre:genre})
+     }     
     
     render() { 
-        let { length:count }=this.state.movies
-        const {pageSize,currentPage,movies:allMovies}=this.state
+        const { length:count }=this.state.movies
+        const {pageSize,currentPage,selectedGenre,movies:allMovies}=this.state
         if (count === 0)
         return <p>there are no movies in the database.</p>
-        const movies = paginate(allMovies,currentPage,pageSize)
+        const filtered = selectedGenre 
+        ? allMovies.filter
+        (m=>m.genre._id === selectedGenre._id):allMovies
+        const movies = paginate(filtered,currentPage,pageSize)
         return (           
-            <main className='container'> 
-            <div className="col-2"></div>
-            <div className="col">
-            <p>showing {count} in the site</p>
-            <listGroup  onClick={this.handleListGroup}/>
+            <div> 
+            <div className="row">
+            <div className="col-3 m-2">
+            <ListGroup 
+            items={this.state.genres} 
+            onItemSelected={this.handleGenreSelected} 
+            selectedItem={this.state.selectedGenre}           
+            />
+            </div>
+            <div className="col m-2">
+            <p>showing {filtered.length} in the site</p>
             <table className="table">
                 <thead>
                     <tr> 
@@ -72,13 +83,14 @@ class Movies extends Component {
             </table>
 
             <Pagination
-             itemsCount={count} 
+             itemsCount={filtered.length}  
              pageSize={pageSize}
              currentPage={currentPage}
              onPageChange={this.handlePageChange}/>
-                </div>            
+                </div>  
+            </div>          
 
-            </main>
+            </div>
         );
     }
 }
