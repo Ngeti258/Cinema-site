31708aef880912c6a48772991b6fd095af4dbc2c
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-06 11:09:28 +0300

    introduced the ComponentDidMount function

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -14,7 +14,7 @@ class Movies extends Component {
         genres:[]
      } 
      componentDidMount(){
-        const genres = [{name:'All Genres'},...getGenres()]
+        const genres = [{_id:'', name:'All Genres'},...getGenres()]
         this.setState({movies:getMovies(),genres})
      }
      handleDelete=(movie)=>{
