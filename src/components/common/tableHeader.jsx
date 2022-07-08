fe43abe446aba1ee9c76e49eefb068d00a0cea3a
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-08 14:39:36 +0300

    added sort icon and pointer

--- src/components/common/tableHeader.jsx
+++ src/components/common/tableHeader.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 //column:array
 class TableHeader extends Component {
+    
     raiseSort=path=>{
         const sortColumn = {...this.props.sortColumn}
       if(sortColumn.path === path)
@@ -11,13 +12,23 @@ class TableHeader extends Component {
          sortColumn.order='asc'
       }
       this.props.onSort(sortColumn)
-    }    
+    } 
+    renderSortIcon=column=>{
+        const {sortColumn}=this.props
+        if(column.path !==sortColumn.path) return null
+        if(sortColumn.order==='asc')return <i className='fa fa sort-asc'></i>
+        return <i className='fa fa-sort-desc'></i>
+    }   
     render() { 
         return (
         <thead>
             <tr>
                 {this.props.columns.map(column=>
-                <th key={column.path||column.key} onClick={()=>this.raiseSort(column.path)}>{column.label}</th>)}
+                <th className='clickable'
+                key={column.path||column.key} 
+                onClick={()=>this.raiseSort(column.path)}>
+                {column.label}{this.renderSortIcon(column)}
+                </th>)}
             </tr>
         </thead>
 
