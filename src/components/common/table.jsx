c3078e15e7a953b72160f96e537684ca065e1116
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-08 14:49:19 +0300

    extracted a table

---
+++ src/components/common/table.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import TableBody from './tableBody';
+import TableHeader from './tableHeader';
+
+const Table = (props) => {
+    const {columns,sortColumn,onSort,data}=props
+    return (
+        <table className="table">
+                <TableHeader columns={columns}  
+                sortColumn={sortColumn} 
+                onSort={onSort}
+                />
+                <TableBody columns={columns} 
+                data={data}
+                />
+                
+            </table>
+    );
+}
+ 
+export default Table;
--- src/components/moviesTable.jsx
+++ src/components/moviesTable.jsx
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import Like from './common/like';
-import TableHeader from './common/tableHeader';
-import TableBody from './common/tableBody';
+import Table from './common/table';
 
 class MoviesTable extends Component {
     raiseSort = path => {
@@ -30,19 +29,12 @@ class MoviesTable extends Component {
     render() { 
         const{movies,onSort,sortColumn}=this.props
     return ( 
-        <table className="table">
-                <TableHeader columns={this.columns}  
-                sortColumn={sortColumn} 
-                onSort={onSort}
-                />
-                <TableBody columns={this.columns} 
-                data={movies}
-                />
-                
-            </table>
+        <Table columns={this.columns} data={movies} 
+        sortColumn={sortColumn}
+        onSort={onSort}/>
     
-      )}
-}
+    )
+}}
 export default MoviesTable
  
  
