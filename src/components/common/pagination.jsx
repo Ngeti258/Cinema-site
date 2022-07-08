e38af3a6f66dcb7d6562b8bfbb2ff7722b72c4f0
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-08 14:05:17 +0300

    rendering cell content

--- src/components/common/pagination.jsx
+++ src/components/common/pagination.jsx
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 
 const Pagination = props => {
     const {itemsCount,pageSize, currentPage,onPageChange}=props
-    console.log(currentPage);
     const pagesCount = Math.ceil(itemsCount/pageSize) 
     if(pagesCount===1) return null
     const pages=_.range(1,pagesCount + 1)
