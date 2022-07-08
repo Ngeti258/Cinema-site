97dd9b073d366471e5e7b4d5660835434e8c4785
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-08 10:15:16 +0300

    extracting TableBody

---
+++ src/components/common/tableBody.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class TableBody extends Component {
+    
+    render() { 
+        return (
+            
+        );
+    }
+}
+ 
+export default TableBody;
