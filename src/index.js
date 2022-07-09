6f5f2c5b7da71f29c916c147d738288957fa7ce8
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-09 09:55:44 +0300

    installed routing dependancies

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css'
@@ -9,7 +10,9 @@ import 'font-awesome/css/font-awesome.css'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
     <App />
+    </BrowserRouter>
 
    
   </React.StrictMode>
