936ee747470578f8be647a37ce504c46ebf2964f
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-09 12:40:22 +0300

    imported  route from rrd

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { Route } from 'react-router-dom';
 import Movies from './components/movies';
 function App() {
   return (
     <div>
     <Movies/>
+    <div className='content'>
+
+    </div>
     
     </div>
   );
