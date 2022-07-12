fbdbd117ab89d1bd2eb4214afdb52dc1473f6092
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-12 10:02:43 +0300

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
-import './App.css';
+
 import { Route,Redirect,Switch} from 'react-router-dom';
 import Movies from './components/movies';
 import Customers from './components/customers';
 import NotFound from './components/notFound';
 import MovieForm from './components/movieForm';
 import Rentals from './components/rentals';
+import NavBar from './components/navbar';
+import './App.css';
+import React from 'react';
 function App() {
   return (
+    <React.Fragment>
+    <NavBar/>
    <main className="container">
     <Switch>
     <Route path='/movies' component={Movies}></Route>
@@ -16,9 +21,9 @@ function App() {
     <Route path='/moviesforms' component={MovieForm}></Route>
     <Redirect from='/' exact to ='/movies'/>
     <Redirect to= '/notFound'/>
-    
     </Switch>
    </main>
+   </React.Fragment>
   );
 }
 
