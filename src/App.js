7e2c5ce50c444a0a8e0cdf6f7ebb5bacc2742945
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-12 09:34:37 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route,Redirect,Switch} from 'react-router-dom';
 import Movies from './components/movies';
 import Customers from './components/customers';
 import NotFound from './components/notFound';
@@ -8,14 +8,14 @@ import Rentals from './components/rentals';
 function App() {
   return (
    <main className="container">
+    <Switch>
     <Route path='/movies' component={Movies}></Route>
     <Route path='/customers' component={Customers}></Route>
     <Route path='/rentals' component={Rentals}></Route>
     <Route path='/not-found' component={NotFound}></Route>
     <Route path='/moviesforms' component={MovieForm}></Route>
-
-
-
+    <Redirect from='/'to ='movies'/>
+    </Switch>
    </main>
   );
 }
