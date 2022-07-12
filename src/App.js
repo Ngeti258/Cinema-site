b6fd0cc2a0b51e1db3127b4c9382ab52f2dc32c4
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-12 09:26:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import './App.css';
 import { Route } from 'react-router-dom';
 import Movies from './components/movies';
+import Customers from './components/customers';
+import NotFound from './components/notFound';
+import MovieForm from './components/movieForm';
+import Rentals from './components/rentals';
 function App() {
   return (
-    <div>
-    <Movies/>
-    <div className='content'>
+   <main className="container">
+    <Route path='/movies' component={Movies}></Route>
+    <Route path='/customers' component={Customers}></Route>
+    <Route path='/rentals' component={Rentals}></Route>
+    <Route path='/not-found' component={NotFound}></Route>
+    <Route path='/moviesforms' component={MovieForm}></Route>
 
-    </div>
-    
-    </div>
+
+
+   </main>
   );
 }
 
