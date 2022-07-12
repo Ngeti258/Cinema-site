87a84dc32868a3eb04746b88175f6a80df31f660
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-12 17:12:23 +0300

    Update loginForm.jsx

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -2,7 +2,14 @@ import React, { Component } from 'react'
 
 class LoginForm extends Component {
     render() { 
-        return <h1>login</h1>;
+        return <div>
+            <h1>login</h1>
+            <form action="">
+                <div className="form-group"><label htmlFor=""></label><input type="text" className="form-control" /></div>
+
+                <div className="form-group"><label htmlFor=""></label><input type="text" className="form-control" /></div>
+            </form>
+        </div>;
     }
 }
  
