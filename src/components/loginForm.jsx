2d29a8007aaac4a6e48aafb5979ed1c6f67eeed9
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-14 22:37:57 +0300

    Update loginForm.jsx

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,14 +1,6 @@
 import React, { Component } from 'react'
 
-class LoginForm extends Component {
-    username=React.createRef()
-    state = {
-        account:{username:'',password:''}
-    }
-
-    // componentDidMount(){
-    //     this.username.current.focus();
-    // }
+class LoginForm extends Component { 
     handleSubmit=e=>{
         
         e.preventDefault();
@@ -17,23 +9,33 @@ class LoginForm extends Component {
     }
     handleChange=e=>{
         const account = {...this.state.account}
-        account.username = e.currentTarget.value;
+        account[''] = e.currentTarget.value;
         this.setState({account})
 
     }
     render() { 
+
+
+        const {account}=this.state
         return <div>
             <h1>login</h1>
             <form onSubmit={this.handleSubmit}>
-                <div className="form-group"><label htmlFor="username">UserName</label>
+                <div className="form-group">
+                <label htmlFor="username">UserName</label>
                 <input 
                 onChange={this.handleChange}
-                value={this.state.account.username} 
-                autoFocus ref={this.username} id="username" 
-                className="form-control" /></div>
+                name='username'
+                value={account.username} 
+                autoFocus 
+                ref={this.username} id="username" 
+                className="form-control" />
+                </div>
 
                 <div className="form-group"><label htmlFor="password">Password</label>
-                <input id="password" className="form-control" /></div>
+                <input id="password"
+                name='password'
+                value={account.password}  
+                className="form-control" /></div>
                 <button className="btn btn-primary">Login</button>
             </form>
         </div>;
