7ba641ac9c6e008136a88f090db8c2d62b030d68
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-14 23:36:45 +0300

    Update input.jsx

--- src/components/common/input.jsx
+++ src/components/common/input.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 
-const Input = ({name,label,value,onchange}) => {
+const Input = ({name,label,value,onChange}) => {
     return ( 
         <div>
             <div className="form-group">
                 <label htmlFor={name}>{label}</label>
                 <input 
-                onChange={onchange}
+                onChange={onChange}
                 id={name}
                 name={name}
                 value={value} 
