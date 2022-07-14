2601b703023f676785ad60cb002543a1f38bedfb
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-14 23:59:18 +0300

    Update loginForm.jsx

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -7,11 +7,18 @@ class LoginForm extends Component {
         errors:{ }
     }
     validate=()=>{
-        return {username:'Username is Required'}
+        const errors={}
+        const {account} = this.state
+        if(account.username.trim()==='')
+        errors.username='Username is required'
+        if(account.password.trim()==='')
+        errors.password='Password is required'
+        return Object.keys(errors).length===0?null:errors
     }
     handleSubmit=e=>{        
         e.preventDefault();
         const errors=this.validate();
+        console.log(errors);
         this.setState({errors})
         if(errors) return;
         console.log('submitted');
