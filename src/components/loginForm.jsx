da5e6dd4a8f6df6609d48ef9a61c45aec42f0ad4
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-18 23:56:04 +0300

    Update loginForm.jsx

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,9 +1,10 @@
 import  Joi  from 'joi-browser';
-import React, { Component } from 'react'
+import React from 'react'
+import Form from './common/form';
 import Input from './common/input';
 
 
-class LoginForm extends Component { 
+class LoginForm extends Form { 
     state={
         data:{username:'',password:''},
         errors:{ }
@@ -11,43 +12,16 @@ class LoginForm extends Component {
     schema={
         username:Joi.string().required().label('Username'),
         password:Joi.string().required().label('Password')
-    }
-    validate=()=>{
-        const options= {abortEarly:false}
-        const {error}=Joi.validate(this.state.data,this.schema,options);
-        if(!error) return null;
-        const errors={}
-        for(let item of error.details)
-        errors[item.path[0]]=item.message;
-        return errors
-
-    }
-    handleSubmit=e=>{        
-        e.preventDefault();
-        const errors=this.validate();        
-        this.setState({errors:errors|| {}})
-        if(errors) return;
+    }  
+    doSubmit=()=>{
         console.log('submitted');
     }
-    validateProperty=({name,value})=>{
-        const obj={[name]:value}
-        const schema={[name]:this.schema[name]}
-        const {error}=Joi.validate(obj,schema)
-        return error ? error.details[0].message:null
+   
 
-    }
-    handleChange=({currentTarget:input})=>{
-        const errors={...this.state.errors}
-        const errorMessage=this.validateProperty(input)
-        if(errorMessage) errors[input.name] = errorMessage
-        else delete errors[input.name]
-        const data = {...this.state.data}
-        data[input.name] =input.value;
-        this.setState({data,errors})
-
-    }
+    
+    
     render() { 
-        const {data,errors }=this.state
+        const {data,errors } =this.state;
         return <div>
             <h1>login</h1>
             <form onSubmit={this.handleSubmit}>
@@ -72,6 +46,6 @@ class LoginForm extends Component {
             </form>
         </div>;
     }
+
 }
- 
 export default LoginForm;
