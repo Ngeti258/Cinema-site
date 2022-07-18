647e65a5b95632e7cea4e2b31e2cd2d29a3c14ba
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-07-19 01:05:24 +0300

    Update form.jsx

--- src/components/common/form.jsx
+++ src/components/common/form.jsx
@@ -1,5 +1,6 @@
 import  { Component } from 'react'
 import  Joi  from 'joi-browser'
+import Input from './input'
 
 class Form extends Component {
     state = { 
@@ -47,6 +48,21 @@ class Form extends Component {
          </button>
         )
                
+    }
+    renderInput(name,label,type='text'){
+        const {data,errors } =this.state;
+
+        return(
+            <Input 
+             type={type}
+             name={name} 
+             value={data[name]}
+             label={label}
+             onChange={this.handleChange}
+             error={errors[name]}
+
+             />
+        )
     }
 
 }
