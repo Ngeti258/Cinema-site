8bc35ddd77d5860324543e50b8d6d6b7d6042b6c
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-13 10:34:36 +0300

    updated the like icon

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import Like from '.common/Like'
+import Like from './common/like';
 import { getMovies } from '../services/fakeMovieService';
 class Movie extends Component {
    state = { 
@@ -7,6 +7,13 @@ class Movie extends Component {
     };
    handleDelete= movie => {
       const movies = this.state.movies.filter(m=>m._id !== movie._id)
+      this.setState({movies})
+   }
+   handleLike = movie => {
+      const movies = [...this.state.movies]
+      const index=movies.indexOf(movie)
+      movies[index]={...movies[index]}
+      movies[index].liked = !movies[index].liked
       this.setState({movies})
    }
    render() {
@@ -29,7 +36,9 @@ class Movie extends Component {
                <td>{movie.genre.name}</td>
                <td>{movie.numberInStock}</td>
                <td>{movie.dailyRentalRate}</td>
-              
+               <td>
+               <Like liked={movie.liked} onClick={()=>this.handleLike(movie)}/>
+               </td>
                <td>
                   <button onClick={()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button>
                </td>              
