45132846969816b2e4b7c7000770652f666d9f91
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-19 22:54:58 +0300

    updated navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,7 +11,7 @@ const NavBar = () => {
     </button>
     <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div className="navbar-nav">
-        <NavLink className="nav-item nav-link" to="/movies">Movies</NavLink>
+        <NavLink className="nav-item nav-link" to="/movies">Home</NavLink>
         <NavLink className="nav-item nav-link" to="/customers">Customers</NavLink>
         <NavLink className="nav-item nav-link" to="/rentals">Rentals</NavLink>
         <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
