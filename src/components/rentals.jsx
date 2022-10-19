acece44a9b9fe70d0f0d1b311a6c0cd323f78ad4
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-19 22:45:18 +0300

    updated rentals component

--- src/components/rentals.jsx
+++ src/components/rentals.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Rentals = () => {
-    return <h1>Rent here</h1>;
+    return <h1>this is the page where the customers will be able to rent a movie</h1>;
 }
  
 export default Rentals;
