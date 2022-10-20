5da8278f486ece4ed6c549b8072dc4f0e5c74e19
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-20 12:44:32 +0300

    added listgroup

--- src/components/common/listgroup.jsx
+++ src/components/common/listgroup.jsx
@@ -1,32 +1,32 @@
-import React from 'react'
+import React from "react";
 
-const ListGroup = props => {
-    const {textProperty,
-        valueProperty,
-        items,
-        onItemSelected,
-        selectedItem
-    } = props;
-    return (
-        <div>
-            <ul className="list-group">
-                {items.map(item=>
-                <li onClick={()=>onItemSelected(item)} 
-                key={item[valueProperty]} 
-                className={item===selectedItem?
-                    "list-group-item active":
-                     "list-group-item"}
-                >
-                {item[textProperty]}
-                </li>
-)}
-            </ul>
-        </div>
-      );
+const ListGroup = ({
+  items,
+  textProperty,
+  valueProperty,
+  selectedItem,
+  onItemSelect
+}) => {
+  return (
+    <ul className="list-group">
+      {items.map(item => (
+        <li
+          onClick={() => onItemSelect(item)}
+          key={item[valueProperty]}
+          className={
+            item === selectedItem ? "list-group-item active" : "list-group-item"
+          }
+        >
+          {item[textProperty]}
+        </li>
+      ))}
+    </ul>
+  );
 };
-ListGroup.defaultProps ={
-    textProperty:'name',
-    valueProperty:'_id'
-}
- 
+
+ListGroup.defaultProps = {
+  textProperty: "name",
+  valueProperty: "_id"
+};
+
 export default ListGroup;
