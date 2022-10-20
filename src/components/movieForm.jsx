32525143eb962098badf163fcbe8a1bdc12a2f58
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-20 12:43:23 +0300

    changes to movieform

--- src/components/movieForm.jsx
+++ src/components/movieForm.jsx
@@ -1,66 +1,84 @@
-import React from 'react'
-import Form from './common/form';
-import Joi from 'joi-browser'
-import { getGenres } from '../services/fakeGenreService'
-import { getMovie, saveMovie } from '../services/fakeMovieService'
+import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
+import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getGenres } from "../services/fakeGenreService";
 
-class MovieForm extends Form{
-    state = {
-        data:{title:'',genreId:'',numberInStock:'',Rate:''},
-        errors:{},
-        genres:[]
-      }
-      schema={
-        _id:Joi.string(),
-        title:Joi.string().required().label('title'),
-        genre:Joi.string().required().label('Genre'),
-        numberInStock:Joi.string().min(0).max(100)
-        .required().label('Number in Stock'),
-        Rate:Joi.string().min(0).max(10)
-        .required().label('Rate')
-      }
-      componentDidMount(){
-        const genres = getGenres();
-        this.setState({genres});
+class MovieForm extends Form {
+  state = {
+    data: {
+      title: "",
+      genreId: "",
+      numberInStock: "",
+      dailyRentalRate: ""
+    },
+    genres: [],
+    errors: {}
+  };
 
-        const movieId = this.props.match.params._id
-        if(movieId === 'new') return;
+  schema = {
+    _id: Joi.string(),
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genreId: Joi.string()
+      .required()
+      .label("Genre"),
+    numberInStock: Joi.number()
+      .required()
+      .min(0)
+      .max(100)
+      .label("Number in Stock"),
+    dailyRentalRate: Joi.number()
+      .required()
+      .min(0)
+      .max(10)
+      .label("Daily Rental Rate")
+  };
 
-        const movie = getMovie(movieId)
-        if(!movie) return this.props.history.replace("/not-found");
-        this.setState({data:this.mapToViewModel(movie)})
-      }
-      mapToViewModel(movie){
-        return{
-            _id:movie._id,
-            title:movie.title,
-            genreId:movie.genre._id,
-            numberInStock:movie.numberInStock,
-            dailyRentalRate:movie.dailyRentalRate
- 
+  componentDidMount() {
+    const genres = getGenres();
+    this.setState({ genres });
 
-        }
-    }
-        doSubmit=()=>{
-            saveMovie(this.state.data);
-            this.props.history.push('/movies')
-        }
-      
-    render() { 
-        return (
-            <div>
-                <h1>Movie Form</h1>
-            <form onSubmit={this.handleSubmit}>
-               {this.renderInput('title','Title')}
-               {this.renderSelect('genre','Genre')}
-               {this.renderInput('numberInStock','Number in Stock')}
-               {this.renderInput('Rate','Rate')}            
-               {this.renderButton('Save')}
-            </form>
-            </div>
+    const movieId = this.props.match.params.id;
+    if (movieId === "new") return;
 
-        );
-    }
+    const movie = getMovie(movieId);
+    if (!movie) return this.props.history.replace("/not-found");
+
+    this.setState({ data: this.mapToViewModel(movie) });
+  }
+
+  mapToViewModel(movie) {
+    return {
+      _id: movie._id,
+      title: movie.title,
+      genreId: movie.genre._id,
+      numberInStock: movie.numberInStock,
+      dailyRentalRate: movie.dailyRentalRate
+    };
+  }
+
+  doSubmit = () => {
+    saveMovie(this.state.data);
+
+    this.props.history.push("/movies");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Movie Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderSelect("genreId", "Genre", this.state.genres)}
+          {this.renderInput("numberInStock", "Number in Stock", "number")}
+          {this.renderInput("dailyRentalRate", "Rate")}
+          {this.renderButton("Save")}
+        </form>
+      </div>
+    );
+  }
 }
- 
+
 export default MovieForm;
