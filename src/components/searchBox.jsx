4f5298ee1b99803f20bfb7d8ea6f3ea4fa9f6d2b
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-20 12:43:51 +0300

    added the searchbox

--- src/components/searchBox.jsx
+++ src/components/searchBox.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const SearchBox = ({value,onChange}) => {
-    return ( 
-        <input
-        type='text'
-        name='query'
-        className='form-control my-3'
-        placeholder='Search...'
-        value={value}
-        onChange={e=>onChange(e.currentTarget.value)}
-        
-        />
-     );
-}
- 
+const SearchBox = ({ value, onChange }) => {
+  return (
+    <input
+      type="text"
+      name="query"
+      className="form-control my-3"
+      placeholder="Search..."
+      value={value}
+      onChange={e => onChange(e.currentTarget.value)}
+    />
+  );
+};
+
 export default SearchBox;
