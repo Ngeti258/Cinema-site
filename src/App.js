c6685f0b16de995abaf973bb990715bb82c14efa
Ngeti <70070716+Ngeti258@users.noreply.github.com>
2022-10-20 12:43:05 +0300

    changes to app.js

--- src/App.js
+++ src/App.js
@@ -1,36 +1,36 @@
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./components/movies";
+import MovieForm from "./components/movieForm";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
+import NavBar from "./components/navbar";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/register";
+import "./App.css";
 
-import { Route,Redirect,Switch} from 'react-router-dom';
-import Movies from './components/movies';
-import Customers from './components/customers';
-import NotFound from './components/notFound';
-import MovieForm from './components/movieForm';
-import Rentals from './components/rentals';
-import NavBar from './components/navbar';
-import LoginForm from './components/loginForm';
-import RegisterForm from './components/register';
-import './App.css';
-import React from 'react';
-function App() {
-  return (
-    <React.Fragment>
-    <NavBar/>
-    <main className="container">
-    <Switch>
-    <Route path='/login' component={LoginForm}/>
-    <Route path='/movies/:id' component={MovieForm}/>
-    <Route path='/movies' component={Movies}/>
-    <Route path='/customers' component={Customers}/>
-    <Route path='/rentals' component={Rentals}/>
-    <Route path='/notFound' component={NotFound}/>
-    <Route path='/movies/new' component={MovieForm}/>
-    <Route path='/register' component={RegisterForm}/>
-
-    <Redirect from='/' exact to ='/movies'/>
-    <Redirect to= '/notFound'/>
-    </Switch>
-   </main>
-   </React.Fragment>
-  );
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar />
+        <main className="container">
+          <Switch>
+            <Route path="/register" component={RegisterForm} />
+            <Route path="/login" component={LoginForm} />
+            <Route path="/movies/:id" component={MovieForm} />
+            <Route path="/movies" component={Movies} />
+            <Route path="/customers" component={Customers} />
+            <Route path="/rentals" component={Rentals} />
+            <Route path="/not-found" component={NotFound} />
+            <Redirect from="/" exact to="/movies" />
+            <Redirect to="/not-found" />
+          </Switch>
+        </main>
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
